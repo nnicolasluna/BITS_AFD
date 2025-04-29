a8a57c17fe4d5f3317342de542eaaf1402fbb342
jluna <jluna@gambarte.com.bo>
2025-04-29 10:43:26 -0400

    first commit

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,10 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { ManualesComponent } from './manuales/manuales.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: 'manuales', component: ManualesComponent },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,20 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { MenuComponent } from './items/menu/menu.component';
+import { MatIconModule } from '@angular/material/icon';
+import { ManualesComponent } from './manuales/manuales.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    MenuComponent,
+    ManualesComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    MatIconModule
   ],
   providers: [],
   bootstrap: [AppComponent]
